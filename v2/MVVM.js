0d2caa3a2342c73c7670424fbc00bbf00b35e1c6
niudazhuang <dengwm666@icloud.com>
2018-08-04 01:35:57 +0800

    -

--- v2/MVVM.js
+++ v2/MVVM.js
@@ -3,7 +3,20 @@ class MVVM {
     this.$el = options.el;
     this.$data = options.data;
 
+    this.proxyData(this.$data)
     new Observer(this.$data);
     new Compile(this.$el, this);
+  }
+  proxyData (data) {
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        get () {
+          return data[key]
+        },
+        set (newValue) {
+          data[key] = newValue
+        }
+      })
+    })
   }
 }
